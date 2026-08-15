8f54fd82ffb90c3ec858ffdf5f59b64f9f32c0aa
KXXN <kwonsumin030716@gmail.com>
2026-08-15 20:02:23 +0900

    http proxy로 교체

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,8 +4,14 @@ const nextConfig: NextConfig = {
 
     allowedDevOrigins: ['222.112.156.77'],
 
-    devIndicators: {
-        position: 'bottom-right',
+    async rewrites() {
+        return [
+            {
+                // 브라우저가 /api/proxy 로 요청하면 Next.js 서버가 대신 GCP HTTP 주소로 토스합니다.
+                source: '/api/proxy/:path*',
+                destination: 'http://35.255.26*',
+            },
+        ];
     },
 };
 
